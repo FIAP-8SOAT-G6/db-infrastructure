fb04e9ea1a924cf7076fcc6f1c6ef78ab72ca4fb
Ana Paula Dezuo <ana.dezuo@rdstation.com>
2024-12-10 18:29:22 -0300

    feat: update test

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   # Configuração do backend para armazenar o estado do Terraform
   backend "s3" {
     bucket = "tdb-terraform-bucket"
-    key    = "soat8-g6/backend/terraform.tfstate"
+    key    = "soat8-g6/db/terraform.tfstate"
     region = "us-east-1"
   }
 }
---
+++ subnets.tf
@@ -0,0 +1,8 @@
+#resource "aws_db_subnet_group" "default" {
+#  name        = "db-subnet-group"
+  #subnets da vpc privada
+#  subnet_ids = [
+#    aws_subnet.example_subnet_1.id,
+#    aws_subnet.example_subnet_2.id
+#  ]
+#}
