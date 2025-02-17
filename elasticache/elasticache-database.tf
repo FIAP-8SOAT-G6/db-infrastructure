2b4dba13e5661d705ff00a577adc6a5faf860343
Vítor Rafael Veeck da Silveira <vrsilveira07@gmail.com>
2025-02-16 22:08:23 -0300

    Update elasticache-database.tf

--- elasticache/elasticache-database.tf
+++ elasticache/elasticache-database.tf
@@ -6,8 +6,7 @@ resource "aws_elasticache_replication_group" "elasticache-valkey" {
   engine_version             = "8.0"
   node_type                  = "cache.t4g.micro"
   parameter_group_name       = "default.valkey8"
-  transit_encryption_enabled = true
-  apply_immediately          = true
+  transit_encryption_enabled = false
 
   security_group_ids = [aws_security_group.elasticache_sg.id]
 
